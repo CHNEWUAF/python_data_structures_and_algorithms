0840d71e2fc247145538fdf77b8a8bb10eaa81fc
ehco1996 <zh19960202@gmail.com>
2018-07-17 11:14:22 +0800

    add dict

---
+++ ehco/dict/dict.py
@@ -0,0 +1,52 @@
+from ehco.hashtable.hashtable_adt import HashTable
+
+
+class DictADT(HashTable):
+
+    def _iter_slot(self):
+        for slot in self._table:
+            if slot not in (HashTable.EMPTY, HashTable.UNUSED):
+                yield slot
+
+    def __setitem__(self, key, value):
+        self.add(key, value)
+
+    def __getitem__(self, key):
+        if key not in self:
+            raise KeyError()
+        else:
+            return self.get(key)
+
+    def items(self):
+        for slot in self._iter_slot():
+            yield (slot.key, slot.value)
+
+    def keys(self):
+        for slot in self._iter_slot():
+            yield slot.key
+
+    def values(self):
+        for slot in self._iter_slot():
+            yield slot.value
+
+
+def test_dict_adt():
+    import random
+
+    d = DictADT()
+
+    d['a'] = 1
+    assert d['a'] == 1
+    d.remove('a')
+
+    assert len(d) == 0
+
+    l = list(range(30))
+    random.shuffle(l)
+    for i in l:
+        d.add(i, i)
+
+    for i in range(30):
+        assert d.get(i) == i
+
+    assert sorted(list(d.keys())) == sorted(l)
