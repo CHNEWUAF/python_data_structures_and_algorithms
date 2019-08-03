b5c4d18fe86dc51f0e6e751d742fe3c8a43ef1f0
ehco1996 <zh19960202@gmail.com>
2019-08-03 12:15:57 +0800

    add lru cache

---
+++ ehco/link_list/lru.py
@@ -0,0 +1,168 @@
+class Node:
+    def __init__(self, value=None, next=None, prev=None):
+        self.value = value
+        self.next = next
+        self.prev = prev
+
+    def __str__(self):
+        return f"<Node: value: {self.value}>"
+
+    __repr__ = __str__
+
+
+class DBL:
+    def __init__(self):
+        node = Node("root")
+        node.prev = node
+        node.next = node
+        self.root = node
+
+        self.lens = 0
+
+    @property
+    def head(self):
+        return self.root.next
+
+    @property
+    def tail(self):
+        return self.root.prev
+
+    def append(self, value):
+        node = Node(value)
+
+        self.tail.next = node
+        node.prev = self.tail
+        self.root.prev = node
+        self.lens += 1
+
+    def append_left(self, value):
+        node = Node(value)
+
+        node.next = self.head
+        self.root.next = node
+        node.prev = self.root
+        self.lens += 1
+
+    def remove(self, node):
+        # NOTE for spec case
+        if node == self.root:
+            return False
+        if node == self.tail:
+            self.root.prev = node.prev
+        if node.next:
+            node.next.prev = node.prev
+        node.prev.next = node.next
+        del node
+        self.lens -= 1
+        return True
+
+    def iter_item(self):
+        cur = self.root.next
+        while cur:
+            yield cur
+            cur = cur.next
+
+    def find(self, value):
+        for node in self.iter_item():
+            if node.value == value:
+                return node
+        return None
+
+    def insert(self, value, new_value):
+        per = self.root
+        for node in self.iter_item():
+            if node.value == value:
+                temp = Node(new_value)
+                per.next = temp
+                temp.prev = per
+                temp.next = node
+                node.prev = temp
+                self.lens += 1
+                return True
+            per = node
+        self.append(new_value)
+
+    def __str__(self):
+        return "->".join([str(node.value) for node in self.iter_item()])
+
+    __repr__ = __str__
+
+
+class LRU:
+    def __init__(self, size=10):
+        self.size = size
+        self._link = DBL()
+        self._cache = dict()
+
+    def _move_to_recent(self, node):
+        if node == self._link.tail:
+            return
+
+        # pop node from link
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        # set node to tail
+        now_tail = self._link.tail
+        now_tail.next = node
+        node.prev = now_tail
+        node.next = None
+        self._link.root.prev = node
+
+    def _append(self, k, v):
+        self._link.append(v)
+        self._cache[k] = self._link.tail
+        # Bind cache key
+        self._link.tail.cache_key = k
+
+    def _expired(self):
+        need_expired = self._link.head
+        self._link.remove(need_expired)
+        del self._cache[need_expired.cache_key]
+
+    @property
+    def now_count(self):
+        return self._link.lens
+
+    def get(self, k):
+        node = self._cache.get(k, None)
+        if not node:
+            return
+        self._move_to_recent(node)
+        return node.value
+
+    def set(self, k, v):
+        if k in self._cache:
+            node = self._cache[k]
+            node.value = v
+            self._move_to_recent(node)
+        else:
+            if self.now_count == self.size:
+                self._expired()
+            self._append(k, v)
+        return True
+
+    def __str__(self):
+        return "->".join([f"{node.cache_key}" for node in self._link.iter_item()])
+
+    __repr__ = __str__
+
+
+def test_lru():
+    lru = LRU(size=10)
+
+    for i in range(10):
+        lru.set(i, i)
+
+    for i in range(10):
+        assert lru.get(i) == i
+
+    print(lru)
+    lru.set(11, 11)
+    assert lru.get(11) == 11
+    print(lru)
+
+    assert lru.get(0) == None
+
+    for i in range(100):
+        lru.set(i, i)
+    print(lru)
